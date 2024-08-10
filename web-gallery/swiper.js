b2e2a6a6374016a61362c51470f92c545fb10aa8
ExrendiL <75134327+ExrendiL@users.noreply.github.com>
2024-08-10 11:24:55 +0300

    added new project

---
+++ web-gallery/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
