214e19cc1d7d2da371beb521df9ae762bbbbfd1e
ExrendiL <75134327+ExrendiL@users.noreply.github.com>
2025-01-10 12:52:50 +0200

    modified project - memory card game

--- memory-card-game/card-game.js
+++ memory-card-game/card-game.js
@@ -73,7 +73,67 @@ const matrixGenerator = (cardValues, size = 4) => {
         `;
     }
     gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    cards = document.querySelectorAll(".card-container");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            if(!card.classList.contains("matched")){
+                card.classList.add("flipped");
+                if(!firstCard){
+                    firstCard = card;
+                    firstCardValue = card.getAttribute("data-card-value");
+                }
+                else{
+                    movesCounter();
+                    secondCard = card;
+                    let secondCardValue = card.getAttribute("data-card-value");
+                    
+                    if(firstCardValue == secondCardValue){
+                        firstCard.classList.add("matched");
+                        secondCard.classList.add("matched");
+                        firstCard = false;
+                        winCount += 1;
+    
+                        if(winCount == Math.floor(cardValues.length / 2)){
+                            result.innerHTML = `<h2>You Won</h2> <h4>Moves: ${movesCount}</h4>`;
+                            stopGame();
+                        }
+                    }
+                    else{
+                        let = [tempFirst, tempSecond] = [firstCard, secondCard];
+                        firstCard = false;
+                        secondCard = false;
+                        let delay = setTimeout(() => {
+                            tempFirst.classList.remove("flipped");
+                            tempSecond.classList.remove("flipped");
+                        }, 900);
+                    }
+                }
+            }           
+        });
+    });
 };
+
+startButton.addEventListener("click", () => {
+    movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+    controls.classList.add("hide");
+    stopButton.classList.remove("hide");
+    startButton.classList.add("hide");
+    interval = setInterval(timeGenerator, 1000);
+    moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+    initializer();
+});
+
+stopButton.addEventListener(
+    "click", 
+    (stopGame = () => {
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+    })
+);
 
 const initializer = () => {
     result.innerText = "";
@@ -81,5 +141,3 @@ const initializer = () => {
     let cardValues = generateRandom(); 
     matrixGenerator(cardValues);
 };
-
-initializer();
